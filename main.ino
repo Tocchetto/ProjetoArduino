a5aaaea2851cfae48b3e2c60c70a65a2c5544e94
Tocchetto <tocchettoo@gmail.com>
2017-06-05 22:18:01 -0300

    Update main.ino

--- main.ino
+++ main.ino
@@ -1,62 +1,53 @@
 #include <Servo.h>             
- 
-Servo servo_1;        
+
+Servo servo_1;
 Servo servo_2;
 Servo servo_3;
 Servo servo_4;
 Servo servo_5;
-Servo servo_6;
+Servo servo_6;        
+int angle = 95;
+int zeroServo1 = 84-90;
+int zeroServo2 = 86-90;
+int zeroServo3 = 96-90;
+int zeroServo4 = 90-90;
+int zeroServo5 = 94-90;
+int zeroServo6 = 88-90;
 
-int angle = 95;    
  
 void setup() 
 {
-  pinMode(13, OUTPUT);
-  digitalWrite(13, HIGH);
-  delay(2500);
-  digitalWrite(13, LOW);
-  servo_1.attach(7);     
-  servo_2.attach(8);
-  servo_3.attach(9);
-  servo_4.attach(10);
-  servo_5.attach(11);
-  servo_6.attach(12);
-  delay(1000);
-  servo_1.write(angle); delay(500);
-  servo_2.write(angle); delay(500);
-  servo_3.write(angle); delay(500);
-  servo_4.write(angle); delay(500);
-  servo_5.write(angle); delay(500);
-  servo_6.write(angle); delay(500);
-  delay(500);
-  digitalWrite(13, HIGH);
+  servo_1.attach(7);
+  servo_2.attach(6);
+  servo_3.attach(5);
+  servo_4.attach(4);
+  servo_5.attach(3);
+  servo_6.attach(2);
 } 
   
 void loop() 
-{ 
-  for(; angle < 180; angle += 1)   
-  {                                  
-    servo_1.write(angle);                
-    servo_2.write(angle);
-    servo_3.write(angle);
-    servo_4.write(angle);
-    servo_5.write(angle);
-    servo_6.write(angle);
-    delay(20);                       
-  } 
- 
-  delay(1000);
-  
-  for(; angle>=10; angle-=1)     
-  {                                
-    servo_1.write(angle);              
-    servo_2.write(angle);
-    servo_3.write(angle);
-    servo_4.write(angle);
-    servo_5.write(angle);
-    servo_6.write(angle);
-    delay(15);                       
-  } 
-
-    delay(500);
+{
+  Serial.begin(9600);
+  for(angle=20; angle < 160; angle += 1)   
+  {
+    Serial.println(angle);
+    servo_1.write(zeroServo1+angle); delay(4);
+    servo_2.write(zeroServo2+(angle*-1)+180);delay(4);
+    servo_3.write(zeroServo3+angle);delay(4);
+    servo_4.write(zeroServo4+(angle*-1)+180);delay(4);
+    servo_5.write(zeroServo5+angle);delay(4);
+    servo_6.write(zeroServo6+(angle*-1)+180);delay(4);
+  }   
+  delay(500);
+  for(angle=160; angle>=20; angle-=1)     
+  {
+    Serial.println(angle);
+    servo_1.write(zeroServo1+angle); delay(4);
+    servo_2.write(zeroServo2+(angle*-1)+180);delay(4);
+    servo_3.write(zeroServo3+angle);delay(4);
+    servo_4.write(zeroServo4+(angle*-1)+180);delay(4);
+    servo_5.write(zeroServo5+angle);delay(4);
+    servo_6.write(zeroServo6+(angle*-1)+180);delay(4);                 
+  }
+  delay(500);
 }
